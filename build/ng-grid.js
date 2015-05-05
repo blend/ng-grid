fe0648649149e7d38afc7043a4a860ac5a4a7447
Ian Lyons <ilyons@gmail.com>
2015-05-05 15:10:56 -0700

    merge master

--- build/ng-grid.js
+++ build/ng-grid.js
@@ -2,7 +2,7 @@
 * ng-grid JavaScript Library
 * Authors: https://github.com/angular-ui/ng-grid/blob/master/README.md 
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)
-* Compiled At: 05/05/2015 15:06
+* Compiled At: 05/05/2015 15:10
 ***********************************************/
 (function(window, $) {
 'use strict';
